41836faccf3cbf4be1524465d0886df09240dff3
kiril <kirill2689@gmail.com>
2023-11-22 19:23:15 +0300

    Changed url params

--- vk_image_uploading.py
+++ vk_image_uploading.py
@@ -4,22 +4,31 @@ import requests
 def upload_image_to_vk_group_wall(
         vk_token, group_id, vk_api, attachment, message
 ):
-    url = f"https://api.vk.com/method/wall.post?owner_id=-{group_id}" \
-          f"&message={message}&attachment={attachment}" \
-          f"&access_token={vk_token}&v={vk_api}"
+    params = {
+        "owner_id": f"-{group_id}",
+        "message": message,
+        "attachment": attachment,
+        "access_token" : vk_token,
+        "v": vk_api,
+    }
 
-    response = requests.post(url)
+    url = f"https://api.vk.com/method/wall.post"
+
+    response = requests.post(url, params=params)
     response.raise_for_status()
 
     return response.json()
 
 
 def upload_image_to_vk_server(vk_token, group_id, vk_api, file_name):
-    server_url = f"https://api.vk.com/method/photos.getWallUploadServer" \
-                     f"?group_id={group_id}" \
-                     f"&access_token={vk_token}&v={vk_api}"
+    params = {
+        "group_id": group_id,
+        "access_token": vk_token,
+        "v": vk_api,
+    }
+    server_url = f"https://api.vk.com/method/photos.getWallUploadServer"
 
-    response = requests.get(server_url)
+    response = requests.get(server_url, params=params)
     upload_url = response.json().get("response").get("upload_url")
 
     with open(file_name, 'rb') as file:
@@ -34,21 +43,31 @@ def upload_image_to_vk_server(vk_token, group_id, vk_api, file_name):
         photo = response_json.get("photo")
         vk_hash = response_json.get("hash")
 
-    wall_save_photo_url = f"https://api.vk.com/method/photos.saveWallPhoto?" \
-                          f"&group_id={group_id}" \
-                          f"&server={server}&hash={vk_hash}&photo={photo}" \
-                          f"&access_token={vk_token}&v={vk_api}"
+    params = {
+        "group_id": group_id,
+        "server": server,
+        "hash": vk_hash,
+        "photo": photo,
+        "access_token": vk_token,
+        "v": vk_api,
+    }
+    wall_save_photo_url = f"https://api.vk.com/method/photos.saveWallPhoto"
 
-    response = requests.post(wall_save_photo_url)
+    response = requests.post(wall_save_photo_url, params=params)
     response.raise_for_status()
     return response.json()
 
 
 def vk_get_groups(vk_token):
-    url = f"https://api.vk.com/method/groups.get?" \
-          f"filter=admin&access_token={vk_token}&v=5.154"
+    params = {
+        "filter": "admin",
+        "access_token": vk_token,
+        "v": "5.154",
+    }
 
-    response = requests.get(url)
+    url = f"https://api.vk.com/method/groups.get"
+
+    response = requests.get(url, params=params)
 
     response.raise_for_status()
     return response.json()
